06bc1c37d6884f40d3a1d3f9f991e9af00b9402d
Raghava73 <raghavendrak663@gmail.com>
2025-11-05 11:09:18 +0530

    Pattern Progarms

---
+++ ZeroOneTraingle.java
@@ -0,0 +1,28 @@
+package com.patterns;
+import java.util.Scanner;
+public class ZeroOneTraingle {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number of Rows");
+		int n = sc.nextInt();
+		zeroOneTraingle(n);
+		sc.close();
+	}
+	public static void zeroOneTraingle(int n) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				if((i+j)%2==0) {
+					System.out.print("1"+" ");
+				}
+				else {
+					System.out.print("0"+" ");
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
