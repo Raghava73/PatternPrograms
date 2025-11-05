1edde6b6af23d8384d772fa6dabef3e74efa2a10
Raghava73 <raghavendrak663@gmail.com>
2025-11-05 10:42:21 +0530

    Pattern Programs

---
+++ Diamond.java
@@ -0,0 +1,41 @@
+package com.patterns;
+
+import java.util.Scanner;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number of Rows");
+		int n = sc.nextInt();
+		diamondPattern(n);
+		sc.close();
+
+	}
+	public static void diamondPattern(int n) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=(2*i)-1;j++) { //2*1=2 2-1=1 star will print here 
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=n; i>=1;i--) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+				
+			}
+			for(int j=1; j<=(2*i)-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
